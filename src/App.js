de646186f0dacf419934bae0690058e9334441b4
Wioleta Tyranska <wioleta.tyranska@gmail.com>
2018-07-08 17:13:34 +0200

    wciecia, usuwanie zbednych cssow etc

--- src/App.js
+++ src/App.js
@@ -1,48 +1,42 @@
-import React, {
-  Component
-} from 'react';
-import List from './List';
-import './App.css';
+import React, { Component } from "react";
+import List from "./List";
+import "./App.css";
 
 class App extends Component {
   state = {
     toDoList: [],
     lastTaskId: 0,
-    details: '',
-    taskDate: '',
-  }
-
+    details: "",
+    taskDate: ""
+  };
 
   addTask = () => {
-    
     let list = this.state.toDoList;
     list[this.state.lastTaskId] = {
       id: this.state.lastTaskId,
       details: this.state.details,
       taskDate: new Date(),
-      detailsVisibility: 'hidden'
-    }
+      detailsVisibility: "hidden"
+    };
 
     let nextTaskId = this.state.lastTaskId + 1;
 
     this.setState({
       toDoList: list,
       lastTaskId: nextTaskId,
-      details: '',
-      taskDate: ''
-    })
-    
-  }
-
+      details: "",
+      taskDate: ""
+    });
+  };
 
   clearAll = () => {
     this.setState({
       toDoList: [],
       lastTaskId: 0
-    })
-  }
+    });
+  };
 
-  deleteCurrent = (taskId) => {
+  deleteCurrent = taskId => {
     if (this.state.lastTaskId === 0) {
       return;
     }
@@ -52,64 +46,72 @@ class App extends Component {
 
     this.setState({
       toDoList: list
-    })
+    });
+  };
 
-  }
-
-  handleInputChange = (event) => {
+  handleInputChange = event => {
     let taskDetail = event.target.value;
 
     this.setState({
       details: taskDetail
-    })
-  }
+    });
+  };
 
-  
-    visibilityToggler = (taskId) => {
-      
+  visibilityToggler = taskId => {
     let list = this.state.toDoList;
-    let visibilityState =  list[taskId].detailsVisibility;
+    let visibilityState = list[taskId].detailsVisibility;
 
-    if (visibilityState === 'hidden') {
-      visibilityState = 'showed';
+    if (visibilityState === "hidden") {
+      visibilityState = "showed";
     } else {
-      visibilityState = 'hidden';
+      visibilityState = "hidden";
     }
 
     list[taskId].detailsVisibility = visibilityState;
 
     this.setState({
       toDoList: list
-    })
+    });
 
     console.log(visibilityState);
-    console.log(taskId)
-    }
-
-
+    console.log(taskId);
+  };
 
   render() {
     return (
       <div className="App">
-      <header>
-      <i className="fas fa-list-alt"></i>
-        <h1>To Do List</h1>
-      </header>
-      <section>
-        <div className="addingContainer">
-          <form>
-            <input type="text" name="taskDetails" value={this.state.details} onChange={this.handleInputChange}></input>
-          </form>
-          <button className="addButton" onClick={this.addTask}>Add</button>
-          <div style={{clear: 'both'}}></div>
-        </div>
+        <header>
+          <i className="fas fa-list-alt" />
+          <h1>To Do List</h1>
+        </header>
+        <section>
+          <div className="addingContainer">
+            <form>
+              <input
+                type="text"
+                name="taskDetails"
+                value={this.state.details}
+                onChange={this.handleInputChange}
+              />
+            </form>
+            <button className="addButton" onClick={this.addTask}>
+              Add
+            </button>
+            <div style={{ clear: "both" }} />
+          </div>
         </section>
         <main>
-        <div className='listContainer'>
-          <List items={this.state.toDoList} delete={this.deleteCurrent} toggler={this.visibilityToggler}/>
-        </div>
+          <div className="listContainer">
+            <List
+              items={this.state.toDoList}
+              delete={this.deleteCurrent}
+              toggler={this.visibilityToggler}
+            />
+          </div>
         </main>
-          <button className="clearButton" onClick={this.clearAll}><i className="fas fa-sync-alt"></i></button>
+        <button className="clearButton" onClick={this.clearAll}>
+          <i className="fas fa-sync-alt" />
+        </button>
       </div>
     );
   }
--- src/List.js
+++ src/List.js
@@ -1,38 +1,34 @@
-import React from 'react';
-import './List.css';
+import React from "react";
+import "./List.css";
 
 const List = props => (
-
   <ul className="itemList">
-    {
-      props.items.map((item, index) => <li key={index}>
-      
-      <div className="containerOfAll">
-      <div className='taskNumber'>
-        <div className='headerContainer'>
-          <div className='showDetails'>
-          <i className="fas fa-caret-down"onClick={() => props.toggler(index)}></i>
+    {props.items.map((item, index) => (
+      <li key={index}>
+        <div className="headerContainer">
+          <div className="showDetails">
+            <i
+              className="fas fa-caret-down"
+              onClick={() => props.toggler(index)}
+            />
           </div>
-          <div className='timeContainer'>
-          {new Intl.DateTimeFormat('pl-PL').format(item.taskDate)}
+          <div className="timeContainer">
+            {new Intl.DateTimeFormat("pl-PL").format(item.taskDate)}
           </div>
-          <div className="titleContainer">
-            Task number {item.id}
+          <div className="titleContainer">Task number {item.id}</div>
+          <div className="iconContainer">
+            <i
+              className="fas fa-check-square"
+              onClick={() => props.delete(index)}
+            />
           </div>
-          <div className='iconContainer'>
-            <i className="fas fa-check-square"  onClick={() => props.delete(index)}></i>
         </div>
-          
-          </div>
-      </div>
-      <div className={'taskDetails' + ' ' + item.detailsVisibility} > {item.details}</div>
-      </div>
-      <div style={{clear: 'both'}}></div>
-       
-      </li>)
-      
-    }
-    
+        <div className={"taskDetails" + " " + item.detailsVisibility}>
+          {item.details}
+        </div>
+        <div style={{ clear: "both" }} />
+      </li>
+    ))}
   </ul>
 );
 
